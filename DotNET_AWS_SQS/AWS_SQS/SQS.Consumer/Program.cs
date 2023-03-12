7691357289c58160a133a9b385468c4dbf638020
Arthur White Paim Finardi <arthurfinardi@gmail.com>
2023-03-12 12:52:49 -0300

    Start project AWS Serverless with .NET.

---
+++ DotNET_AWS_SQS/AWS_SQS/SQS.Consumer/Program.cs
@@ -0,0 +1,14 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ DotNET_AWS_SQS/AWS_SQS/SQS.Supplier/Program.cs
@@ -0,0 +1,14 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Supplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
