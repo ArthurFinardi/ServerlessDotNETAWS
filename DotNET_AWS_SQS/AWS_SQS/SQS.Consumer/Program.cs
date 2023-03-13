54ca02f8031efad94191ee8338258af3398eecda
Arthur White Paim Finardi <arthurfinardi@gmail.com>
2023-03-12 22:45:09 -0300

    Creating an SQS queue and testing its functionality

--- DotNET_AWS_SQS/AWS_SQS/SQS.Consumer/Program.cs
+++ DotNET_AWS_SQS/AWS_SQS/SQS.Consumer/Program.cs
@@ -6,9 +6,24 @@ namespace SQS.Consumer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1); // SP Region 
+            var request = new ReceiveMessageRequest
+            {
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/367945316125/teste1"
+            };
+            var response = await client.ReceiveMessageAsync(request);
+            while(true)
+            {
+                foreach (var message in response.Messages)
+                {
+                    Console.WriteLine(message.Body);
+                    client.DeleteMessageAsync("https://sqs.sa-east-1.amazonaws.com/367945316125/teste1", message.ReceiptHandle);
+                }
+            }
         }
     }
 }
--- DotNET_AWS_SQS/AWS_SQS/SQS.Supplier/Program.cs
+++ DotNET_AWS_SQS/AWS_SQS/SQS.Supplier/Program.cs
@@ -6,9 +6,17 @@ namespace SQS.Supplier
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1); // SP Region 
+            var request = new SendMessageRequest
+            {
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/367945316125/teste1",
+                MessageBody = "teste 123"
+            };
+            await client.SendMessageAsync(request);
         }
     }
 }
